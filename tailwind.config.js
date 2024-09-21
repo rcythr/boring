7c29afd54932fd2b7e786a5d1d6afd450f1ae908
Richard Laughlin <richardwlaughlin@gmail.com>
2024-09-21 19:23:34 -0400

    Customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,25 +2,25 @@ const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html", "./theme/**/*.html"],
-  darkMode: 'class',
-  theme: {
-    container: {
-      center: true,
-      screens: {
-        lg: '800px',
-        xl: '900px',
-        '2xl': '900px',
+  content :
+          [
+            './templates/**/*.html', './theme/**/*.html',
+            '../../content/**/*.md'
+          ],
+  darkMode : 'class',
+  theme : {
+    container : {
+      center : true,
+      screens : {
+        lg : '800px',
+        xl : '900px',
+        '2xl' : '900px',
       },
     },
-    fontFamily: {
-      serif: ['"Crimson Pro"', ...defaultTheme.fontFamily.serif],
-      sans: ['"Work Sans"', ...defaultTheme.fontFamily.sans],
-      mono: ['"JetBrains Mono"', ...defaultTheme.fontFamily.mono],
-    },
   },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  variants : {},
+  plugins :
+          [
+            require('@tailwindcss/typography'),
+          ],
 };
